7f68704d89ae13c4c574bc26136ea492ae377216
rvndrupal <rodrigodrupal1@gmail.com>
2019-05-03 22:04:38 -0500

    Php POO

---
+++ TODO_PHP/Curso_POO/04.PDOySQL/editado/views/modules/registro.php
@@ -0,0 +1,32 @@
+<h1>REGISTRO DE USUARIO</h1>
+
+<form method="post">
+	
+	<input type="text" placeholder="Usuario" name="usuarioRegistro" required>
+
+	<input type="password" placeholder="Contraseña" name="passwordRegistro" required>
+
+	<input type="email" placeholder="Email" name="emailRegistro" required>
+
+	<input type="submit" value="Enviar">
+
+</form>
+
+<?php
+
+//PRUEBA DE LOS CAMBIOS
+
+$registro = new MvcController();
+$registro -> registroUsuarioController();
+
+if(isset($_GET["action"])){
+
+	if($_GET["action"] == "ok"){
+
+		echo "Registro Exitoso";
+	
+	}
+
+}
+
+?>
